52d7feb49e47ced4b965ef700639e242fd7a8d78
Juan Pablo Castellanos Torres <juanptorres2003@gmail.com>
2022-05-08 08:35:37 -0500

    se dio permiso para poder manipularlo desde otra direccion

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.use(cors(corsOptions));
 const port = 4000;
 require("dotenv").config();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(route);
